e69f40c5c2be1792e911d20c1adce188bed83a01
Alex Dowsett <alexdowsett@Alexs-MacBook-Air-2.local>
2025-06-05 16:46:20 +0100

    Added Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 import {NextUIProvider} from "@nextui-org/react";
@@ -7,7 +8,8 @@ import {NextUIProvider} from "@nextui-org/react";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <NextUIProvider>
-    <App />
+        <App />
+        <Analytics />
       </NextUIProvider>
   </StrictMode>,
 )
